4ec7e8e60f526dae2781c4a05a0d534b7a0db551
paizaga <79244304+paizaga@users.noreply.github.com>
2021-07-17 22:27:00 -0400

    ll

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -14,9 +14,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var customTip: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var tipSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -28,10 +30,16 @@ class ViewController: UIViewController {
         
         let bill = Double (billAmountTextField.text!) ?? 0
         
-        let tipPercentages = [0.15, 0.18, 0.20]
+        let custom = Double (customTip.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.20, custom/100]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
         let total = bill + tip
         
+       // if tipSlider.value > 0 {
+         //   let tipPercentage = Double(tipSlider.value)
+           // total = bill + (bill * tipPercentage)
+        //}
         tipAmountLabel.text = String (format: "$%.2f", tip)
         totalLabel.text = String (format: "$%.2f", total)
     }
